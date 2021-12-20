71ccbea5aa54a75116661243aafc488bd01e8479
luiz fernando <luiz123jfmg@gmail.com>
2021-12-20 14:10:49 -0300

    Começando projeto

---
+++ src/main/java/br/lf/ProjetoBancarioJunitApplication.java
@@ -0,0 +1,13 @@
+package br.lf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancarioJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancarioJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/lf/ProjetoBancarioJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancarioJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
